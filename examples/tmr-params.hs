db6c81ffe4999441ab2574e54fdfd36d945bb50a
Patrick Pelletier <code@funwithsoftware.org>
2017-04-15 14:06:25 -0700

    need to retry the create as well as the connect

--- examples/tmr-params.hs
+++ examples/tmr-params.hs
@@ -5,28 +5,31 @@ import Control.Monad
 import qualified Data.Text as T
 import qualified System.Hardware.MercuryApi as TMR
 
-repeatedConnect :: TMR.Reader -> Int -> IO ()
-repeatedConnect rdr n
-  | n <= 1 = TMR.connect rdr
-  | otherwise = do
-      eth <- try (TMR.connect rdr)
-      case eth of
-        Left exc ->
-          case TMR.meStatus exc of
-            TMR.ERROR_TIMEOUT -> do
-              print (TMR.meStatus exc)
-              repeatedConnect rdr (n - 1)
-            TMR.ERROR_DEVICE_RESET -> do
-              print (TMR.meStatus exc)
-              repeatedConnect rdr (n - 1)
-            _ -> throwIO exc
-        _ -> return ()
+data DoWhat = Return | Throw | Retry deriving (Eq)
+
+createAndConnect :: T.Text -> Int -> IO TMR.Reader
+createAndConnect uri n = do
+  rdr <- TMR.create uri
+  eth <- try (TMR.connect rdr)
+  let (doWhat, e) = case eth of
+                      Left exc ->
+                        case TMR.meStatus exc of
+                          TMR.ERROR_TIMEOUT -> (Retry, exc)
+                          TMR.ERROR_DEVICE_RESET -> (Retry, exc)
+                          _ -> (Throw, exc)
+                      _ -> (Return, undefined)
+      doWhat' = if n <= 1 && doWhat == Retry
+                then Throw
+                else doWhat
+  when (doWhat' /= Return) $ TMR.destroy rdr
+  case doWhat' of
+    Throw -> throw e
+    Retry -> createAndConnect uri (n - 1)
+    Return -> return rdr
 
 main = do
-  putStrLn "create"
-  rdr <- TMR.create "tmr:///dev/ttyUSB0"
-  putStrLn "connect"
-  repeatedConnect rdr 100
+  putStrLn "createAndConnect"
+  rdr <- createAndConnect "tmr:///dev/ttyUSB0" 100
   putStrLn "paramList"
   params <- TMR.paramList rdr
   putStrLn "destroy"
