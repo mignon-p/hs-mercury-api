65d8e9e24d37388f5af15e342fc1ea2adebc8a44
Patrick Pelletier <code@funwithsoftware.org>
2017-04-15 22:19:20 -0700

    a transport listener that matches serialPrinter in the C samples

--- examples/tmr-params.hs
+++ examples/tmr-params.hs
@@ -3,14 +3,29 @@
 import Control.Concurrent
 import Control.Exception
 import Control.Monad
+import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified System.Hardware.MercuryApi as TMR
+import Text.Printf
+
+listener :: TMR.TransportListener
+listener tx dat _ = lstn dat (prefix tx)
+  where
+    prefix True  = "Sending: "
+    prefix False = "Received:"
+    lstn bs pfx = do
+      let (bs1, bs2) = B.splitAt 16 bs
+          hex = concatMap (printf " %02x") (B.unpack bs1)
+      putStrLn $ pfx ++ hex
+      when (not $ B.null bs2) $ lstn bs2 "         "
 
 main = do
   putStrLn "create"
   rdr <- TMR.create "tmr:///dev/ttyUSB0"
-  putStrLn "connectRepeatedly"
-  TMR.connectRepeatedly rdr 100
+  putStrLn "addTransportListener"
+  TMR.addTransportListener rdr listener
+  putStrLn "connect"
+  TMR.connect rdr
   putStrLn "paramList"
   params <- TMR.paramList rdr
   putStrLn "destroy"
