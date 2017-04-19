2cbabbd257982566421464a1978bb57afaea8005
Patrick Pelletier <code@funwithsoftware.org>
2017-04-18 23:11:59 -0700

    print some string parameters

--- examples/tmr-params.hs
+++ examples/tmr-params.hs
@@ -5,6 +5,7 @@ import Control.Exception
 import Control.Monad
 import qualified Data.ByteString as B
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.Word
 import qualified System.Hardware.MercuryApi as TMR
 import Text.Printf
@@ -20,19 +21,35 @@ listener tx dat _ = lstn dat (prefix tx)
       putStrLn $ pfx ++ hex
       when (not $ B.null bs2) $ lstn bs2 "         "
 
+stringParams :: [TMR.Param]
+stringParams =
+  [ TMR.PARAM_VERSION_HARDWARE
+  , TMR.PARAM_VERSION_SERIAL
+  , TMR.PARAM_VERSION_MODEL
+  , TMR.PARAM_VERSION_SOFTWARE
+  , TMR.PARAM_URI
+  , TMR.PARAM_PRODUCT_GROUP
+  , TMR.PARAM_READER_DESCRIPTION
+  , TMR.PARAM_READER_HOSTNAME
+  ]
+
 main = do
   putStrLn "create"
   rdr <- TMR.create "tmr:///dev/ttyUSB0"
   putStrLn "addTransportListener"
   TMR.addTransportListener rdr listener
-  putStrLn "paramGet"
+  putStrLn "paramGet PARAM_TRANSPORTTIMEOUT"
   timeout <- TMR.paramGet rdr TMR.PARAM_TRANSPORTTIMEOUT :: IO Word32
   print timeout
   putStrLn "connect"
   TMR.connect rdr
   putStrLn "paramList"
   params <- TMR.paramList rdr
-  putStrLn "destroy"
-  TMR.destroy rdr
   forM_ params $ \param -> do
     putStrLn $ show param ++ " - " ++ T.unpack (TMR.paramName param)
+  forM_ stringParams $ \param -> do
+    putStrLn $ "paramGet " ++ show param
+    txt <- TMR.paramGet rdr param
+    T.putStrLn txt
+  putStrLn "destroy"
+  TMR.destroy rdr
