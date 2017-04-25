34ba2384000138aca1e0bd39ed7f5d88ae994b76
Patrick Pelletier <code@funwithsoftware.org>
2017-04-25 22:44:48 +0000

    some debugging

--- examples/tmr-read.hs
+++ examples/tmr-read.hs
@@ -16,15 +16,24 @@ main = do
   rdr <- TMR.create "tmr:///dev/ttyUSB0"
   listener <- TMR.hexListener stdout
   TMR.addTransportListener rdr listener
+  TMR.paramSet rdr TMR.PARAM_TRANSPORTTIMEOUT (10000 :: Word32)
   TMR.connect rdr
 
+  putStrLn "setting parameters"
   plan <- TMR.paramGet rdr TMR.PARAM_READ_PLAN
+  putStrLn "setting region"
   TMR.paramSet rdr TMR.PARAM_REGION_ID TMR.REGION_NA2
+  putStrLn "setting read power"
   TMR.paramSet rdr TMR.PARAM_RADIO_READPOWER (500 :: Int32)
   let plan' = plan { TMR.rpAntennas = [1] }
+  putStrLn "setting plan"
   TMR.paramSet rdr TMR.PARAM_READ_PLAN plan'
 
-  tags <- TMR.read rdr 500
-  mapM_ T.putStrLn $ concatMap TMR.displayTagReadData tags
+  putStrLn "reading tags"
+  forM_ [1..10] $ \_ -> do
+    tags <- TMR.read rdr 500
+    putStrLn "here are the tags:"
+    mapM_ T.putStrLn $ concatMap TMR.displayTagReadData tags
+    putStrLn ""
 
   TMR.destroy rdr
