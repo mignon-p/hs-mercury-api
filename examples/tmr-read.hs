01cc507f032439f156ad133430e73cf99a95b387
Patrick Pelletier <code@funwithsoftware.org>
2017-05-18 13:30:43 -0700

    command-line options for tmr-read

--- examples/tmr-read.hs
+++ examples/tmr-read.hs
@@ -9,17 +9,39 @@ import Data.Monoid
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Word
+import Options.Applicative
 import System.Console.ANSI
 import qualified System.Hardware.MercuryApi as TMR
 import System.IO
 
-readAll =
-  TMR.TagOp_GEN2_ReadData
-  { TMR.opBank = TMR.GEN2_BANK_TID
-  , TMR.opExtraBanks = [TMR.GEN2_BANK_EPC, TMR.GEN2_BANK_TID, TMR.GEN2_BANK_USER]
-  , TMR.opWordAddress = 0
-  , TMR.opLen = 32
+data Opts = Opts
+  { oUri :: String
+  , oPower :: Int32
+  , oListen :: Bool
   }
+
+opts :: Parser Opts
+opts = Opts
+  <$> strOption (long "uri" <>
+                 short 'u' <>
+                 metavar "URI" <>
+                 help ("Reader to connect to (default " ++ defUri ++ ")") <>
+                 value defUri)
+  <*> option auto (long "power" <>
+                   short 'p' <>
+                   metavar "CENTI-DBM" <>
+                   help ("Power level (0-2700, default " ++ show defPower ++ ")") <>
+                   value defPower)
+  <*> switch (long "transport-listener" <>
+              short 't' <>
+              help "Print bytes sent on serial port")
+  where
+    defUri = "tmr:///dev/ttyUSB0"
+    defPower = 2300
+
+opts' = info (helper <*> opts)
+  ( fullDesc <>
+    header "tmr-read - read tags" )
 
 readUser =
   TMR.TagOp_GEN2_ReadData
@@ -37,18 +59,17 @@ printInColor xs n = do
   setSGR [Reset]
 
 main = do
-  rdr <- TMR.create "tmr:///dev/ttyUSB0"
-  listener <- TMR.hexListener stdout
-  TMR.addTransportListener rdr listener
+  o <- execParser opts'
+
+  rdr <- TMR.create $ T.pack $ oUri o
+  when (oListen o) $ do
+    listener <- TMR.hexListener stdout
+    void $ TMR.addTransportListener rdr listener
   TMR.paramSet rdr TMR.PARAM_TRANSPORTTIMEOUT (10000 :: Word32)
   TMR.connect rdr
 
-  TMR.paramSet rdr TMR.PARAM_REGION_ID TMR.REGION_NA2
-  -- TMR.paramSet rdr TMR.PARAM_RADIO_READPOWER (500 :: Int32)
-  let plan = TMR.defaultReadPlan { TMR.rpTagop = Just readUser
-                                 , TMR.rpAntennas = TMR.sparkFunAntennas
-                                 }
-  TMR.paramSet rdr TMR.PARAM_READ_PLAN plan
+  TMR.paramSetBasics rdr TMR.REGION_NA2 (oPower o) TMR.sparkFunAntennas
+  TMR.paramSetReadPlanTagop rdr (Just readUser)
 
   tags <- TMR.read rdr 1000
   putStrLn $ "read " ++ show (length tags) ++ " tags"
