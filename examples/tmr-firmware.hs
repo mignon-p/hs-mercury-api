75b6c4b792d37c7157016184b3550cddc3057c44
Patrick Pelletier <code@funwithsoftware.org>
2017-05-19 11:13:37 -0700

    use ExampleUtil for tmr-firmware

--- examples/tmr-firmware.hs
+++ examples/tmr-firmware.hs
@@ -9,36 +9,47 @@ import Data.Monoid
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Word
+import Options.Applicative
 import qualified System.Hardware.MercuryApi as TMR
 import System.Environment
 import System.Exit
 import System.IO
+
+import ExampleUtil
+
+data Opts = Opts
+  { oUri :: String
+  , oListen :: Bool
+  , oFilename :: Maybe String
+  }
+
+opts :: Parser Opts
+opts = Opts
+  <$> optUri
+  <*> optListen
+  <*> optional (argument str (metavar "FILENAME"))
+
+opts' = info (helper <*> opts)
+  ( fullDesc <>
+    progDesc ("If run without firmware file, just print current " ++
+              "firmware version.  " ++
+              "Firmware file can be downloaded from: " ++
+              "http://www.thingmagic.com/index.php/download-nano-firmware") <>
+    header "tmr-firmware - write a new firmware image to reader" )
 
 printFwVers :: TMR.Reader -> IO ()
 printFwVers rdr =
   TMR.paramGet rdr TMR.PARAM_VERSION_SOFTWARE >>= T.putStrLn
 
 main = do
-  args <- getArgs
-  let argsLen = length args
-  when (argsLen < 1 || argsLen > 2) $ do
-    putStrLn "Usage: tmr-firmware URI [firmware-file]"
-    putStrLn "Example: tmr-firmware tmr:///dev/ttyUSB0 NanoFW-1.7.1.2.sim"
-    putStrLn "Firmware file can be downloaded from:"
-    putStrLn "http://www.thingmagic.com/index.php/download-nano-firmware"
-    putStrLn "If run without firmware file, just print current firmware version."
-    exitFailure
-  let uri = args !! 0
-
-  rdr <- TMR.create (T.pack uri)
-  putStrLn $ "connecting to " ++ uri
-  TMR.connect rdr
+  o <- execParser opts'
+  rdr <- createAndConnect (oUri o) (oListen o)
 
   putStr "Current firmware version: "
   printFwVers rdr
 
-  when (argsLen < 2) exitSuccess
-  let fwFile = args !! 1
+  when (oFilename o == Nothing) exitSuccess
+  let (Just fwFile) = oFilename o
 
   putStrLn $ "Loading firmware image from " ++ fwFile
   TMR.firmwareLoadFile rdr fwFile
